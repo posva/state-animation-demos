c4672e3d34cbd3590164b15051a78304257d1a5f
Eduardo San Martin Morote <posva13@gmail.com>
2018-05-24 11:35:40 +0200

    use names

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Vue.js Amsterdam Demos',
+    title: 'Vue.js State Animations Demos',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
